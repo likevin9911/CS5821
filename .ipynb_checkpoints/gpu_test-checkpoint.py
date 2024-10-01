cc39b0243879751137101a0b337f468165f65eeb
Kevin Li <kgli@mtu.edu>
2024-10-01 13:40:41 -0400

    HW1 notebooks

---
+++ .ipynb_checkpoints/gpu_test-checkpoint.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+print("TensorFlow Version:", tf.__version__)
+print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
+print("GPUs:", tf.config.list_physical_devices('GPU'))
